f68257ff7aa13dcea29f14ee1bc0d3c7a36d18ea
AbdAhmad <abdullahahmad00786@gmail.com>
2022-01-31 23:55:58 +0530

    made search bar expanding left

--- blog_app/static/js/main.js
+++ blog_app/static/js/main.js
@@ -1,30 +1,3 @@
-// function signupPasswordOneVisibilityToggle() {
-//     let passwordOne = document.getElementById("passwordOne");
-//         if (passwordOne.type === "password") {
-//             passwordOne.type = "text";
-//         } else {
-//             passwordOne.type = "password";
-//         }
-//     }
-
-// function signupPasswordTwoVisibilityToggle() {
-//     let passwordTwo = document.getElementById("passwordTwo");
-//     if (passwordTwo.type === "password") {
-//         passwordTwo.type = "text";
-//     } else {
-//         passwordTwo.type = "password";
-//     }
-// }
-
-// function loginPasswordVisibilityToggle() {
-//     let myInput = document.getElementById("myInput");
-//         if (myInput.type === "password") {
-//             myInput.type = "text";
-//         } else {
-//             myInput.type = "password";
-//         }
-// }
-
 var message_ele = document.getElementById("message_container");
 var signup_errors = document.getElementById("signup-errors");
 
